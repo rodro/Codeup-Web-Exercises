54f237a7256d19e0095de2a17285391562d136ac
rodros <Rodro@Rodros-MacBook-Pro-3.local>
2017-06-23 17:03:25 -0500

    third week Final day of calc work

--- public/js/calc-other.js
+++ public/js/calc-other.js
@@ -1,48 +1,35 @@
-
-//Labels
-// vars for all the keys in my calc
+// Get all the keys from document
 var keys = document.querySelectorAll('#calculator span');
-var operators = ['+', '-', 'x', '÷', '+/-' ,'%'];
+var operators = ['+', '-', 'x', '÷'];
 var decimalAdded = false;
 
-
-//action parts
-// the onclick event to all the "keys" var and perform operations
+// Add onclick event to all the keys and perform operations
 for(var i = 0; i < keys.length; i++) {
 	keys[i].onclick = function(e) {
-		// grabs the input and button values and puts it in the .screen class
+		// Get the input and button values
 		var input = document.querySelector('.screen');
 		var inputVal = input.innerHTML;
 		var btnVal = this.innerHTML;
 		
-
-
-		// clear key is pressed, erase everything
+		// Now, just append the key values (btnValue) to the input string and finally use javascript's eval function to get the result
+		// If clear key is pressed, erase everything
 		if(btnVal == 'C') {
 			input.innerHTML = '';
 			decimalAdded = false;
 		}
 		
-		
-		// eval key is pressed
-
-		// eval key is pressed, calculate and display the result on my .screen
+		// If eval key is pressed, calculate and display the result
 		else if(btnVal == '=') {
 			var equation = inputVal;
 			var lastChar = equation[equation.length - 1];
 			
-			// swap out of x and ÷ with * and / 
+			// Replace all instances of x and ÷ with * and / respectively. This can be done easily using regex and the 'g' tag which will replace all instances of the matched character/substring
 			equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
-			equation = equation.replace(/%/g, '(-var)');
-			//  clearing out the last char in case of decimal
+			
+			// Final thing left to do is checking the last character of the equation. If it's an operator or a decimal, remove it
 			if(operators.indexOf(lastChar) > -1 || lastChar == '.')
 				equation = equation.replace(/.$/, '');
-		
-			if(btnVal == '.posneg')
-			input.innerHTML = inputVal * [-1];
-			decimalAdded = false;
-		
-
+			
 			if(equation)
 				input.innerHTML = eval(equation);
 				
